f4172db78247fc8ed3a5c391cc08bd4912f8ee64
ZanderP <ZanderP@HAMMPC01YHC2.global.bdfgroup.net>
2018-07-13 14:29:00 +0200

    Assignment Week 5

---
+++ GitHubSpielwiese/src/OpenSAP/Week5Assignment.java
@@ -0,0 +1,34 @@
+package OpenSAP;
+
+public class Week5Assignment {
+
+	public static void main(String[] args) {
+		
+		/*
+		// TODO Auto-generated method stub
+		int start = 2012; 
+		int end = 2017; 
+		double cases = 27;
+		System.out.println("Hello, I'm Duke.  From " + start + " to " + end + ", "
+				+ "I solved " + ( cases / (end - start)) 
+				+ " cases on average per year.");
+		
+		
+		
+		int x; 
+		
+		x = Integer.parseInt("1337");	
+		System.out.println(x);
+	 * 
+	 */
+		
+		Vehicle test = new Vehicle();
+		System.out.println(Vehicle.count);
+		Vehicle test2 = new Vehicle();
+		System.out.println(Vehicle.count);
+		
+		
+		
+	}
+
+}
