5fdc8fe3f162793e655936234a64b74b3269ff2d
ZanderP <ZanderP@HAMMPC01YHC2.global.bdfgroup.net>
2018-07-13 13:45:58 +0200

    test

--- GitHubSpielwiese/src/GitTestPackage/HelloGit.java
+++ GitHubSpielwiese/src/GitTestPackage/HelloGit.java
@@ -7,7 +7,7 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 		System.out.println("First edit..");
 		System.out.println("Added Change ID");
-		System.out.println("Test new Branch 2.0");
+		System.out.println("Test new Branch 2.0 --");
 		
 		Car test = new Car(1996);
 		System.out.println(test.getYear());
