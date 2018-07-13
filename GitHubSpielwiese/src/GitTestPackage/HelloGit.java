c3f3ef4cc91073436f2018c52e687a082fef6639
ZanderP <ZanderP@HAMMPC01YHC2.global.bdfgroup.net>
2018-07-13 13:39:19 +0200

    Change ID added
    Change-Id: Id98729ca9b54b59d2f5725759eb8e70e446a2c3b

--- GitHubSpielwiese/src/GitTestPackage/HelloGit.java
+++ GitHubSpielwiese/src/GitTestPackage/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
 		System.out.println("First edit..");
+		System.out.println("Added Change ID.");
 		
 		Car test = new Car(1996);
 		System.out.println(test.getYear());
